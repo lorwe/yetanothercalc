84f9533d5ca758fd4d59891476030ba133be80f9
farid <farid@info74.ru>
2016-05-25 20:10:07 +0500

    Initial Commit

---
+++ YetAnotherCalculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YetAnotherCalculator
+//
+//  Created by Farid on 25.05.16.
+//  Copyright © 2016 Farid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YetAnotherCalculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YetAnotherCalculator
+//
+//  Created by Farid on 25.05.16.
+//  Copyright © 2016 Farid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

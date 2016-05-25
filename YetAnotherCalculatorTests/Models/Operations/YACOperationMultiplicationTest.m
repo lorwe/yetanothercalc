46e2b34ae71fe4017fae9492b84de61814c9b9fe
farid <farid@info74.ru>
2016-05-25 22:36:11 +0500

    Added YACOperationMultiplication class

---
+++ YetAnotherCalculatorTests/Models/Operations/YACOperationMultiplicationTest.m
@@ -0,0 +1,61 @@
+//
+//  YACOperationMultiplicationTest.m
+//  YetAnotherCalculator
+//
+//  Created by Farid on 25.05.16.
+//  Copyright (c) 2016 Farid. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#import "YACOperationProtocol.h"
+#import "YACOperationMultiplication.h"
+
+@interface YACOperationMultiplicationTest : XCTestCase
+
+@end
+
+@implementation YACOperationMultiplicationTest
+
+- (void)testIfItAddsNumbers {
+	id <YACOperationProtocol> operation = [YACOperationMultiplication new];
+	NSArray *arguments = @[@8, @2];
+
+	NSNumber *result = [operation evaluateWithArguments:arguments];
+
+	XCTAssertEqualObjects(result, @16);
+}
+
+- (void)testIfItThrowsInvalidArgumentExceptionWithSingleArgument {
+	id <YACOperationProtocol> operation = [YACOperationMultiplication new];
+	NSArray *arguments = @[@8];
+
+	XCTAssertThrowsSpecificNamed([operation evaluateWithArguments:arguments], NSException, NSInvalidArgumentException);
+}
+
+- (void)testIfItThrowsInvalidArgumentExceptionOnInvalidArgument {
+	id <YACOperationProtocol> operation = [YACOperationMultiplication new];
+	NSArray *arguments1 = @[@"foo", @2];
+	NSArray *arguments2 = @[@2, @"foo"];
+
+	XCTAssertThrowsSpecificNamed([operation evaluateWithArguments:arguments1], NSException, NSInvalidArgumentException);
+	XCTAssertThrowsSpecificNamed([operation evaluateWithArguments:arguments2], NSException, NSInvalidArgumentException);
+}
+
+- (void)testIfItReturnsPriority {
+	id <YACOperationProtocol> operation = [YACOperationMultiplication new];
+
+	NSInteger priority = operation.priority;
+
+	XCTAssertEqual(priority, 2);
+}
+
+- (void)testIfItReturnsNumberOfArguments {
+	id <YACOperationProtocol> operation = [YACOperationMultiplication new];
+
+	NSInteger numberOfArguments = operation.numberOfArguments;
+
+	XCTAssertEqual(numberOfArguments, 2);
+}
+
+
+@end
